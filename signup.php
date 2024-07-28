7964a5a28b3673c91c6b8d92279db37a80de4213
meryem zriouil <meryem.zriouil@uit.ac.ma>
2024-07-28 23:30:55 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,5 @@
 <?php
-include 'connexionf.php'; // Include the database connection script
-
+include 'connexionf.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
@@ -9,16 +8,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $birthdate = $_POST['birthdate'];
     $password = $_POST['password'];
     $confirm_password = $_POST['confirm_password'];
-
-    // Validate that the password and confirm password match
     if ($password !== $confirm_password) {
         die("Passwords do not match.");
     }
-
-    // Hash the password
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    // Prepare and execute the SQL statement
     $stmt = $conn->prepare("INSERT INTO USER (first_name, last_name, phone, email, birthdate, password) VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->bind_param("ssssss", $first_name, $last_name, $phone, $email, $birthdate, $hashed_password);
 
