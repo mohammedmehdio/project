09fdaa67119bb2a97f02e387d46fe544bdf92afe
mohammedmehdio <mohammed-mehdi.boudir@uit.ac.ma>
2024-07-30 01:34:55 +0100

    trying to connect with php

--- php/connexionf.php
+++ php/connexionf.php
@@ -1,12 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "inscription_clubhub"; 
+$username = "root";
+$password = "";
+$dbname = "inscription_clubhub";
+
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully to the database!";
 }
 ?>
